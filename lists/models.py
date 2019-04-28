0ea04e77900b8c3b04fc70f1acb6d963c265d0a0
Stavros Avramidis <stavros9899@gmail.com>
2019-04-28 19:00:16 +0300

    field cannot be unique and blank
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- lists/models.py
+++ lists/models.py
@@ -118,7 +118,7 @@ class Pc(models.Model):
 class Monitor(models.Model):
     price = models.IntegerField(blank=False)
     name = models.CharField(max_length=64, blank=False, unique=True)
-    url = models.URLField(blank=True, unique=True)
+    url = models.URLField(blank=True)
     resolution = models.CharField(
         max_length=16,
         blank=False,

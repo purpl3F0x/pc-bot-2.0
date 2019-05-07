22c8dd0586f94e30830eed634eb606d629d1ff72
Stavros Avramidis <stavros9899@gmail.com>
2019-05-07 18:35:22 +0300

    Initial Commit of V4, Created app parts
    Signed-off-by: Stavros Avramidis <stavros9899@gmail.com>

--- lists/admin.py
+++ lists/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Helper, Monitor, Pc, Peripheral, Tag, UserBuild
+from .models import *
 
 
 # Register your models here.
--- pc_bot/settings.py
+++ pc_bot/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
     'lists',
     'discord_admin',
+    'parts',
 ]
 
 MIDDLEWARE = [
